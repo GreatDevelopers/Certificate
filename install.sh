63c9f6e3a215a14d3863e978ae2c18ccc5e64797
Ranvir Singh <ranvir.singh1114@gmail.com>
2016-10-08 21:14:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 sudo apt-get install apache2 php unoconv python3-uno 
 cp -rf ../Certificate/ /var/www/html
-cd /var/www
 chown -R $USER:www-data /var/www/html/Certificate
 chmod -R 755 /var/www/html/Certificate
