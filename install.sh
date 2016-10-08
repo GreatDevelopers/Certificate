d62c414e575558e97b027f912b000f75ec05b520
singh1114 <ranvir.singh1114@gmail.com>
2016-10-09 00:45:41 +0530

    No error coming but still can't see the certificate

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install apache2 php unoconv python3-uno 
+sudo apt-get install apache2 php unoconv python3-uno php-xml
 cp -rf ../Certificate/ /var/www/html
 chown -R $USER:www-data /var/www/html
 chmod -R 755 /var/www/html
