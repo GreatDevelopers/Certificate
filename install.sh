09efd5a1e3445a0431e28162b249075d24c128e5
Ranvir Singh <ranvir.singh1114@gmail.com>
2016-10-14 00:03:12 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 sudo apt-get install apache2 php unoconv python3-uno php-xml
-<<<<<<< HEAD
 sudo cp -rf ../Certificate/ /var/www/html
 sudo chown -R $USER:www-data /var/www/html
 sudo chmod -R 775 /var/www/html
-=======
-cp -rf ../Certificate/ /var/www/html
-chown -R $USER:www-data /var/www/html
-chmod -R 775 /var/www/html
->>>>>>> b04f61e58caa655ab4c4b82b4fe54ec548ea1b72
