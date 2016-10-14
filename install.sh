a9772acf16e622a19e8e7414c93e8a92a8299a5a
singh1114 <ranvir.singh1114@gmail.com>
2016-10-14 11:10:24 +0530

    another library added for running php

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install apache2 php unoconv python3-uno php-xml php-gd
+sudo apt-get install apache2 php unoconv python3-uno php-xml php-gd libapache2-mod-php
 sudo cp -rf ../Certificate/ /var/www/html
 sudo chown -R $USER:www-data /var/www/html
 sudo chmod -R 775 /var/www/html
