fe714973da158d0f35aeaf6ccc0d55d72a87add8
Mandeep Singh <mandeeps708@gmail.com>
2017-09-26 13:58:33 -0400

    Restructured project

--- csv.php
+++ csv.php
@@ -1,6 +1,6 @@
 <?php session_start();
 
-require_once('../library/odf.php');
+require_once('library/odf.php');
 
   $id = uniqid();				//To be used with filenames to differentiate simultaneous files being processed 
   $csv = $id.$_FILES["file"]["name"];
