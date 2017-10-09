8230b52b495eb6285bc474e0430a91b9b2413b25
amisha2016 <amishabudhiraja96@gmail.com>
2017-10-09 13:08:24 +0530

    Code when no image selected

--- manual_no_image.php
+++ manual_no_image.php
@@ -2,97 +2,7 @@
 session_start();
 require_once('library/odf.php');
 require_once('decide.php');
-//$base = $_SESSION["base"]; 				//Getting file name with filled Institute Details
-//$odf = new odf("odt/base/$base.odt");   		//Initializing the object with above file name
-//$id = uniqid();
-//$_SESSION['id'] = $id;					//To be used with filenames to differentiate simultaneous files being processed
 
-/*
-if ($_SERVER['REQUEST_METHOD'] == 'POST')  
-{
-// Assigning Form data to sesssion variables to be used in next step.
-
-$_SESSION['sal'] = $_POST["sal"];
-$_SESSION['fname'] = $_POST["fname"];
-$_SESSION['mname'] = $_POST["mname"];
-$_SESSION['lname'] = $_POST["lname"];
-$_SESSION['ins'] = $_POST["ins"];
-$_SESSION['city'] = $_POST["city"];
-$_SESSION['state'] = $_POST["state"];
-
-*/
-//assigning image name to variable photo.
-
-//$photo = "$id".strtok($_FILES["file"]["name"],".");	//using strtok() to store filename without extension
-//$_SESSION['photo'] = $photo;				//Assigning Photo variable to session vatriable
-
-/*************************************** Image Validation********************************************/
-
-// Link to the other file if any of following condition fails
-/*
-$url = "<meta http-equiv='Refresh' content='3; URL=option.php?var=manual'>"; 
-
-
-if($_FILES["file"]["name"] == Null)		//checks if no file is selected
-	{
-	  echo "<center><strong>No Image Selected!</strong></center>";
-	  echo $url;
-	  exit;
-	}
-
-
-if($_FILES["file"]["size"] > 400144) 		//Size validation Condition(should be less than 400kb)
-	{
-	  echo "<center><strong>Image Size Exceeded...</strong></center>";
-	  echo $url;
-	  exit;
-	}
-
-//moving the uploaded file to directory
-move_uploaded_file($_FILES["file"]["tmp_name"],"uploads/manual/" .$photo);
-
-$photo_path = "uploads/manual/$photo";		//storing path of uploaded photo in variable
-
-//Checking the type of uploaded Image file
-
-if ($_FILES["file"]["type"] == "image/gif")
-	$origin = imagecreatefromgif($photo_path);
-
-elseif($_FILES["file"]["type"] == "image/jpeg")
-	$origin = imagecreatefromjpeg($photo_path);
-
-elseif($_FILES["file"]["type"] == "image/jpg")
-	$origin = imagecreatefromjpeg($photo_path);
-
-elseif($_FILES["file"]["type"] == "image/png")
-	$origin = imagecreatefrompng($photo_path);
-
-//In case of any other format of uploaded file
-else
-	{
-	  echo "<center><strong>Invalid Image File...</strong></center>";
-	  echo $url;
-	  exit;
-	}
-
-$originWidth = imagesx($origin);   //Width of original uploaded image
-$originHeight = imagesy($origin);  //Height of original uploaded image
-
-//creating another image to which the original image is copied
-$destination = imageCreateTrueColor( $originWidth, $originHeight );
-
-//Copying the original uploaded image to new created one
-imagecopyresampled($destination,$origin,0,0,0,0,$originWidth,$originHeight,$originWidth,$originHeight);
-
-//Finally saving the image as jpeg in directory to be used by cropping tool
-imagejpeg($destination,"uploads/manual/src.jpg",100);
-
-}
-
-
-else 
-{
-*/
 //Using Session variables to fetch form data
 	$name = $_SESSION['sal'];
 	$fname = $_SESSION['fname'];
@@ -104,36 +14,11 @@ else
 	$photo = $_SESSION['photo'];
 	$id = $_SESSION['id'];
   
-/*      
-	$targ_w = $targ_h = 500;	//Dimensions of cropped image
-	$jpeg_quality = 100;		//Quality of cropped image
-
-	$src = "uploads/manual/src.jpg";
-
-	//defining handles for creating the crooped image
-	$img_r = imagecreatefromjpeg($src);		
-	$dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
-	
-	//Generating the image using co-ordinates represented by the Crop Box
-	imagecopyresampled($dst_r,$img_r,0,0,$_GET['x'],$_GET['y'],
-	$targ_w,$targ_h,$_GET['w'],$_GET['h']);
-	
-	//saving the jpeg image after cropping
-	imagejpeg($dst_r,"uploads/manual/cropped/$photo",$jpeg_quality);
-*/
 
 $article = $odf->setSegment('articles');	//Defining Segment articles( used in .odt file)
 	
 		 //image
-/*  
-              $pic = "uploads/manual/cropped/".$photo;
-                
-                if(!file_exists($pic))
-                  {
-                  $pic = "uploads/manual/image.gif";
-                 }
-		$article->setImage('pic',$pic,4);
-*/		
+
 $article->pic(" ");
 		//name
                  if($mname==NULL)
@@ -193,5 +78,4 @@ echo   '<html>
 	</html>';
 
 exit;
-//}
 ?>
